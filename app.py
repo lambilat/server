d092fdba0d8085ee45a729d88dd8249dfe07d662
bilat <lambilat@proton.me>
2023-06-23 14:17:41 +0800

    ge

--- app.py
+++ app.py
@@ -18,17 +18,35 @@ def convert_datetime(timestamp):
 app = Flask(__name__)
 
 url_dict = {
-    'https://google.com':{
+    'https://www009.vipanicdn.net/streamhls/0b594d900f47daabc194844092384914/ep.1.1677592419.m3u8':{
         'status':'',
-        'created':'',
+        'created':'02:00 PM June 23',
         'failed_dt':''
     },
-    'http://gagukabatanga.com/':{
+    'https://delivery238.akamai-video-content.com/hls2/01/02979/3pf7o29i349d_,l,n,h,.urlset/master.m3u8?t=Dzvl6MVOdqdspu4Mr4AF5tn7C3DHSm7JHxZnmGHY54g&s=1687500188&e=10800&f=14898541&srv=sto225':{
         'status':'',
-        'created':'',
+        'created':'02:03 PM June 23',
+        'failed_dt':''
+    },
+    'https://tc-1.moocdn.net/_v6/daa1b0f081a8d4fe589619f7ab99f1567b3c666c95e9ee62ecf34d7f48101aca4021f5676ce7795285562b26a48a457d5c979a536e6cee29307198926bc60673914a5be80f5c2907f5620c810f7abaa48d0378a8cb4f8f3e5879c74ff816cd30c882ec66b1e76130bc63aae2f9da50619d40863a3c0b3eaaa282b89600c29176/master.m3u8':{
+        'status':'',
+        'created':'02:13 PM June 23',
+        'failed_dt':''
+    },
+    'https://tc-1.boocdn.net/_v6/daa1b0f081a8d4fe589619f7ab99f1567b3c666c95e9ee62ecf34d7f48101aca4021f5676ce7795285562b26a48a457d5c979a536e6cee29307198926bc60673914a5be80f5c2907f5620c810f7abaa48d0378a8cb4f8f3e5879c74ff816cd30c882ec66b1e76130bc63aae2f9da50619d40863a3c0b3eaaa282b89600c29176/master.m3u8':{
+        'status':'',
+        'created':'02:15 PM June 23',
+        'failed_dt':''
+    },
+    'https://c-an-ca4.betterstream.cc:2223/hls-playback/daa1b0f081a8d4fe589619f7ab99f1567b3c666c95e9ee62ecf34d7f48101aca4021f5676ce7795285562b26a48a457d593fe3a0a91be642006fb1735b9c723a5c3cd0404cec66430d4ce4f8351a12fb7046652193d6d641833ccc005575934f125e038e20c54be236db37fdc6978a6e83a413812da32f0f943d2ca4569207f14055c10f3553ce4d145b03b1802b2213/master.m3u8':{
+        'status':'',
+        'created':'02:15 PM June 23',
         'failed_dt':''
     }
 }
+
+
+
 
 
 
@@ -39,7 +57,7 @@ def check_url():
     for url in url_dict:
         is_response = False
         if not url_dict[url]['status'] == 'failed':
-            print(" Checking:",url)
+            print(" Checking:",url[0:50])
             try:
                 response = requests.get(url, timeout=10)
                 if response.status_code == 200:
